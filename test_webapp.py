1cdfbe2f87f9896373874b64dd579420c671833e
Robin Kåveland Hansen <rkh@knowit.no>
2013-12-17 20:13:57 +0100

    Tester for webapp.

---
+++ test_webapp.py
@@ -0,0 +1,34 @@
+import webapp
+import json
+
+def test_httpify_should_prepend_http_to_adress_without_http():
+    assert webapp.httpify('vg.no') == 'http://vg.no'
+
+def test_httpify_should_not_prepend_when_adress_already_has_http():
+    assert webapp.httpify('http://vg.no') == 'http://vg.no'
+
+def test_httpify_should_strip_whitescpace():
+    assert webapp.httpify(' http://vg.no ') == 'http://vg.no'
+
+def test_get_hello_world_is_a_json_response_that_returns_200_ok():
+    response = webapp.app.test_client().get('/hello-world')
+    assert response.content_type == 'application/json'
+    assert json.loads(response.data) == {'message': 'Hello, World!'}
+
+@mock.patch('text.generate', return_value=['text'])
+@mock.patch('html_parse.gather_headlines')
+def test_get_urls_should_generate_200_ok_response(gather_headlines, generate):
+    response = webapp.app.test_client().get('/vg.no,db.no')
+    assert response.status_code == 200
+
+@mock.patch('text.generate', return_value=['text'])
+@mock.patch('html_parse.gather_headlines')
+def test_get_urls_should_fetch_webpages(gather_headlines, generate):
+    response = webapp.app.test_client().get('/vg.no,db.no')
+    gather_headlines.assert_called_with(['http://vg.no', 'http://db.no'])
+
+@mock.patch('text.generate', return_value=['text'])
+@mock.patch('html_parse.gather_headlines')
+def test_get_urls_should_generate_text_and_return_it_as_a_json_object(gather_headlines, generate):
+    response = webapp.app.test_client().get('/vg.no,db.no')
+    assert json.loads(response.data) == {'generated': ['text']}
