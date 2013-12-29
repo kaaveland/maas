c93b9cec4d0490e982d83927abc8284fcaca9942
Robin Kåveland Hansen <rkh@knowit.no>
2013-12-29 11:57:53 +0100

    Adding working solution to text.py

--- text.py
+++ text.py
@@ -2,6 +2,7 @@
 For learning from text and generating text.
 """
 import random
+from collections import defaultdict
 
 # For a 'prefix'-type that is central to an easy markov-chain algorithm.
 # It is used to index words into the markov chain, both for assignment and lookup.
@@ -9,18 +10,17 @@ import random
 
 def shift(word, prefix):
     """Shift word onto prefix such that word is last and the current first word disappears."""
-    pass
+    return prefix[1:] + (word,)
 
 def last_word(prefix):
     """Returns the last word in prefix."""
-    pass
+    return prefix[-1]
 
 def null_prefix(length=1):
     """A null-prefix is 'before' the start of a text, like ^ in a regex.
 
     It is 'empty' or not initialized."""
-
-    pass
+    return tuple([None for _ in range(length)])
 
 # Training algorithm to take a list of words and output a chain.
 # A chain is a mapping from prefixes to all possible words that may follow a prefix.
@@ -36,7 +36,22 @@ def find_prefixes(words, length=1):
       shifted_shifted_null_prefix: [words known to follow this prefix]
     }
     """
-    pass
+    chain = defaultdict(list)
+    prefix = null_prefix(length)
+    for word in words:
+        chain[prefix].append(word)
+        prefix = shift(word, prefix)
+    return chain
+
+def find_prefixes(words, length=1):
+    """Alternate solution using regular dict instead of defaultdict."""
+    chain = {}
+    prefix = null_prefix(length)
+    for word in words:
+        # Set chain[prefix] = [] if it not prefix in chain then append to chain[prefix]
+        chain.setdefault(prefix, []).append(word)
+        prefix = shift(word, prefix)
+    return chain
 
 def merge(*chains):
     """Merge several chains into one chain containing the same data.
@@ -44,29 +59,33 @@ def merge(*chains):
     The effect should be that the result contains the training data from all the chains
     provided as arguments.
     """
-    pass
+    merged_chain = defaultdict(list)
+    for chain in chains:
+        for prefix, suffixes in chain.items():
+            merged_chain[prefix].extend(suffixes)
+    return merged_chain
 
 def chain_from(sentences, length=1):
     """Return a chain that has been built by using it on every sentence in sentences.
 
     Sentences is a list of strings."""
-    pass
+    return merge(*[find_prefixes(sentence, length) for sentence in sentences])
 
 # Generating text
 
 def choice(alternatives):
     """Choose a random alternative from alternatives."""
-    pass
+    return random.choice(alternatives)
 
 def can_continue(prefix, chain):
     """False when we don't know of any continuation for prefix."""
-    pass
+    return chain.get(prefix)
 
 def next_word(prefix, chain):
     """Choose the next word based on our current prefix and chain.
 
     Only words that are known to follow prefix can be chosen."""
-    pass
+    return choice(chain[prefix])
 
 def generate(chain, length=1, max_words=100):
     """Generates up to `max_words` words of text based on `chain`, using a prefix of order `length`.
@@ -74,4 +93,20 @@ def generate(chain, length=1, max_words=100):
     We need to have a max_words to prevent infinite loops, a chain such as:
     {'foo': ['bar'], 'bar': ['foo']}
     Is entirely legal (and quite likely to happen at some point)."""
-    pass
+    words = []
+    prefix = null_prefix(length)
+    for _ in range(max_words):
+        if can_continue(prefix, chain):
+            word = next_word(prefix, chain)
+            words.append(word)
+            prefix = shift(word, prefix)
+    return ' '.join(words)
+
+# When run with $ python text.py
+if __name__ == '__main__':
+    with open('dracula_chapter1.txt') as book:
+        text = book.read()
+        sentences = [[word.strip() for word in sentence.split()]
+                     for sentence in text.split('.')]
+        chain = chain_from(sentences, 2)
+        print generate(chain, 2)
